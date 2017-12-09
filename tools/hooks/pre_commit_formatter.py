1ebeab4498266965de9ac6def5335e8aa83cf8ee
Filippo Costa <filippocosta.italy@gmail.com>
2017-12-09 21:55:37 +0100

    Speed up pre-commit hook

--- tools/hooks/pre_commit_formatter.py
+++ tools/hooks/pre_commit_formatter.py
@@ -9,16 +9,16 @@ from yapf.yapflib.yapf_api import FormatFile
 def format_sources():
     git_status = subprocess.check_output(
         ["git", "status", "--porcelain", "-uno"], stderr=subprocess.STDOUT)
-    git_status_lines = [
+    file_names = [
         file_name[3:] for file_name in git_status.decode("utf-8").splitlines()
     ]
     print("Running code formatters...")
-    for file_name in git_status_lines:
+    for file_name in file_names:
         if file_name.endswith((".c", ".h", ".cpp", ".cc", ".hpp")):
             subprocess.call(["clang-format", "-i", file_name[3:]])
         elif file_name.endswith(".py"):
             FormatFile(file_name, in_place=True)
-        subprocess.call(["git", "add", file_name])
+    subprocess.call(["git", "add"] + file_names)
 
 
 if __name__ == "__main__":
