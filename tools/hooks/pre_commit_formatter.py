01b250f45c1b50168cae9982347412d45eb663ac
Filippo Costa <filippocosta.italy@gmail.com>
2017-12-17 11:10:40 +0100

    Refactor the pre-commit hook

--- tools/hooks/pre_commit_formatter.py
+++ tools/hooks/pre_commit_formatter.py
@@ -1,27 +1,86 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 import subprocess
 import sys
-from yapf.yapflib.yapf_api import FormatFile
+
+try:
+    from yapf.yapflib.yapf_api import FormatFile
+    YAPF = True
+except ImportError:
+    YAPF = False
+
+EXTENSIONS_CXX = (".c", ".h", ".cpp", ".cc", ".hpp")
+EXTENSIONS_PY = (".py", )
+EXTENSIONS = EXTENSIONS_CXX + EXTENSIONS_PY
+
+NEEDS_FORMATTING = lambda x: x.endswith(EXTENSIONS)
 
 
-def format_sources():
-    git_status = subprocess.check_output(
-        ["git", "status", "--porcelain", "-uno"], stderr=subprocess.STDOUT)
-    file_names = [
-        file_name[3:] for file_name in git_status.decode("utf-8").splitlines()
-    ]
-    if file_names:
-        print("Running code formatters...")
+def print_formatting_notice(file_name):
+    print("Formatting {}...".format(file_name))
+
+
+def print_formatting_success(file_name):
+    print("Formatting {} -- SUCCESS".format(file_name))
+
+
+def print_formatting_fail(file_name):
+    print("Formatting {} -- FAILED".format(file_name))
+
+
+def format_python(file_name):
+    if YAPF:
+        print_formatting_notice(file_name)
+        FormatFile(file_name, in_place=True)
+        print_formatting_success(file_name)
+    return YAPF
+
+
+def format_cxx(file_name):
+    if shutil.which("clang-format") is not None:
+        print_formatting_notice(file_name)
+        try:
+            subprocess.call(["clang-format", "-i", file_name], shell=True)
+            print_formatting_success(file_name)
+            return True
+        except subprocess.CalledProcessError:
+            print_formatting_fail(file_name)
+            return False
+
+
+def format_sources(file_names):
+    modified_file_names = []
     for file_name in file_names:
-        if file_name.endswith((".c", ".h", ".cpp", ".cc", ".hpp")):
-            subprocess.call(["clang-format", "-i", file_name])
-        elif file_name.endswith(".py"):
-            FormatFile(file_name, in_place=True)
-    subprocess.call(["git", "add"] + file_names)
+        if file_name.endswith(EXTENSIONS_CXX):
+            success = format_cxx(file_name)
+        elif file_name.endswith(EXTENSIONS_PY):
+            success = format_python(file_name)
+        if success:
+            modified_file_names.append(file_name)
+    return modified_file_names
+
+
+def git_add_file_names(file_names):
+    try:
+        subprocess.check_output(["git", "add"], stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        pass
+
+
+def git_status_file_names():
+    git_status_params = ["git", "status", "--porcelain", "-uno"]
+    try:
+        git_status = subprocess.check_output(
+            git_status_params, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        return []
+    git_status_lines = git_status.decode("utf-8").splitlines()
+    return [line[3:] for line in git_status_lines]
 
 
 if __name__ == "__main__":
-    format_sources()
+    file_names = [x for x in git_status_file_names() if NEEDS_FORMATTING(x)]
+    git_add_file_names(format_sources(file_names))
     sys.exit(0)
