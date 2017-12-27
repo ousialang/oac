315b8b1f6b33e07bad86d4d2586ee1b4bc21bd82
Filippo Costa <filippocosta.italy@gmail.com>
2017-12-27 23:00:33 +0100

    Include stdint in scanner.h

--- ousiac/include/scanner.h
+++ ousiac/include/scanner.h
@@ -1,51 +1,53 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
+#include <stdint.h>
+
 struct token_t;
 
 typedef struct {
-  bool is_alpha;
+	bool is_alpha;
 } token_word_t;
 
 typedef struct {
-  uint8_t base;
-  char *digits;
+	uint8_t base;
+	char* digits;
 } token_integer_t;
 
 typedef struct {
-  char *integer_digits;
-  char *decimal_digits;
+	char* integer_digits;
+	char* decimal_digits;
 } token_decimal_t;
 
 typedef enum {
-  ROUND,
-  SQUARE,
-  CURLY,
+	ROUND,
+	SQUARE,
+	CURLY,
 } bracket_t;
 
 typedef struct {
-  char bracket;
-  struct token_t **tokens;
+	char bracket;
+	struct token_t** tokens;
 } token_block_t;
 
 typedef enum {
-  WORD,
-  INTEGER,
-  DECIMAL,
-  BLOCK,
+	WORD,
+	INTEGER,
+	DECIMAL,
+	BLOCK,
 } token_type_t;
 
 typedef union {
-  token_integer_t integer;
-  token_decimal_t decimal;
-  token_block_t block;
+	token_integer_t integer;
+	token_decimal_t decimal;
+	token_block_t block;
 } token_info_t;
 
 typedef struct {
-  token_type_t type;
-  token_info_t *info;
-  uint64_t index;
-  uint16_t length;
+	token_type_t type;
+	token_info_t* info;
+	uint64_t index;
+	uint16_t length;
 } token_t;
 
 #endif
