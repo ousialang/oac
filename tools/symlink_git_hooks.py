fd94a1d72f11c9b489cde71b8e7836ac0ef20e5e
Filippo Costa <filippocosta.italy@gmail.com>
2018-02-25 14:33:40 +0200

    Fix non-working git hook

--- tools/symlink_git_hooks.py
+++ tools/symlink_git_hooks.py
@@ -6,13 +6,11 @@ if __name__ == "__main__":
     here = os.path.realpath(
         os.path.join(os.getcwd(), os.path.dirname(__file__)))
     hook_destination = os.path.join(
-        os.path.dirname(here), ".git", "git_hooks", "pre-commit")
+        os.path.dirname(here), ".git", "hooks", "pre-commit")
     hook_location = os.path.join(here, "git_hooks", "pre_commit_formatter.py")
     try:
         os.remove(hook_destination)
-        try:
-            os.symlink(hook_location, hook_destination)
-        except (FileNotFoundError, OSError) as e:
-            pass
     except (FileNotFoundError, OSError) as e:
         pass
+    # FIXME: possible race condition
+    os.symlink(hook_location, hook_destination)
