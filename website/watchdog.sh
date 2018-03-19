f13aebc701d5e4dff3bdafead22190d19acaf7eb
Filippo Costa <filippocosta.italy@gmail.com>
2018-03-19 20:53:58 +0200

    Remove watchdog.sh in favor of Gulp builds

--- website/watchdog.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# HOW TO RUN:
-#   $ cd oa/website
-#   $ sh watchdog.sh
-
-export FLASK_APP=index.py
-export FLASK_DEBUG=1
-python3 -m flask run #& sass --watch assets/scss:static/css
