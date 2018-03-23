75ba7ad2f83fd7d9f90bfd9cdd8db8e158169502
Filippo Costa <filippocosta.italy@gmail.com>
2018-03-23 15:49:28 +0200

    Nuke the C++ tests

--- oa/test/path.cpp
+++
@@ -1,9 +0,0 @@
-#include "catch.hpp"
-#include "utils/path.hpp"
-
-TEST_CASE("File paths shortening works as expected.", "[feedback]") {
-	char* s1 = "src/abc/foobar_123/spam/ousia.oa:1337:42";
-	char* s2 = "";
-	char* s3 = "x";
-	char* s4 = "a/b/c/d.oa:1:2";
-}
