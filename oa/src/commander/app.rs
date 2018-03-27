4d742857531c9e4ba232d337880a9f83e48a6e25
Filippo Costa <filippocosta.italy@gmail.com>
2018-03-27 23:14:09 +0200

    Remove half-baked dead app module

--- oa/src/commander/app.rs
+++
@@ -1,9 +0,0 @@
-use self::clap::{App, AppSettings};
-
-
-const APP: App = App::new("oa")
-    .setting(AppSettings::AllowExternalSubcommands)
-    .setting(AppSettings::StrictUtf8)
-    .setting(AppSettings::VersionlessSubcommands)
-    .setting(AppSettings::DisableHelpSubcommand)
-    .setting(AppSettings::DisableVersion);
