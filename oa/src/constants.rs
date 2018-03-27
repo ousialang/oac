9953a5af27d7d6a4d7df1c21fed26d828721a9a4
Filippo Costa <filippocosta.italy@gmail.com>
2018-03-27 20:41:19 +0200

    Spread the OusiaVersion struct fields across the module

--- oa/src/constants.rs
+++ oa/src/constants.rs
@@ -2,26 +2,12 @@ use std::env::var_os;
 use std::path::PathBuf;
 
 
-pub struct OusiaVersion {
-    pub major: u16,
-    pub minor: u16,
-    pub patch: u16,
-    pub tags: Vec<&'static str>,
-    pub hash: &'static str,
-    pub timestamp_rfc3339: &'static str,
-}
-
-pub fn OUSIA_VERSION() -> OusiaVersion {
-    OusiaVersion {
-        major: env!("CARGO_PKG_VERSION_MAJOR").parse().unwrap(),
-        minor: env!("CARGO_PKG_VERSION_MINOR").parse().unwrap(),
-        patch: env!("CARGO_PKG_VERSION_PATCH").parse().unwrap(),
-        tags: env!("CARGO_PKG_TAGS").split("-").collect(),
-        hash: env!("CARGO_PKG_HASH"),
-        timestamp_rfc3339: env!("CARGO_PKG_TIMESTAMP_RFC3339"),
-    }
-}
-
+pub const VERSION_MAJOR: &'static str = env!("CARGO_PKG_VERSION_MAJOR");
+pub const VERSION_MINOR: &'static str = env!("CARGO_PKG_VERSION_MINOR");
+pub const VERSION_PATCH: &'static str = env!("CARGO_PKG_VERSION_PATCH");
+pub const VERSION_TAGS: Option<&'static str> = option_env!("CARGO_PKG_VERSION_TAGS");
+pub const VERSION_HASH: &'static str = env!("CARGO_PKG_HASH");
+pub const VERSION_TIMESTAMP_RFC3339: &'static str = env!("CARGO_PKG_TIMESTAMP_RFC3339");
 
 pub fn OUSIA_PATH_FROM_ENV() -> Option<PathBuf> {
     var_os("OUSIA_PATH").map(|s| PathBuf::from(s))
--- oa/src/subcommands/version.rs
+++ oa/src/subcommands/version.rs
@@ -1,4 +1,4 @@
-use constants::{OUSIA_VERSION, OusiaVersion};
+use constants;
 use utils::feedback::Level;
 
 use clap::ArgMatches;
@@ -9,29 +9,29 @@ use exitcode::ExitCode;
 pub fn main(matches: ArgMatches) -> ExitCode {
     let version = OUSIA_VERSION();
     match matches.args.len() {
-        0 => print_human_readable_version(version),
-        _ => print_machine_readable_version(version),
+        0 => print_human_readable_version(),
+        _ => print_machine_readable_version(),
     }
     exitcode::OK
 }
 
-fn print_human_readable_version(version: OusiaVersion) {
-    let version_hyphenated_tags = {
-        if version.tags.is_empty() {
+fn print_human_readable_version() {
+    let hyphenated_tags = {
+        if constants::VERSION_TAGS.is_empty() {
             "".to_owned()
         } else {
-            version.tags.join("-")
+            constants::VERSION_TAGS.join("-")
         }
     };
     println!("Ousia {}.{}.{}{} ({})",
-        version.major,
-        version.minor,
-        version.patch,
-        version_hyphenated_tags,
-        version.hash,
+        constants::VERSION_MAJOR,
+        constants::VERSION_MINOR,
+        constants::VERSION_PATCH,
+        hyphenated_tags,
+        constants::VERSION_HASH,
     );
 }
 
-fn print_machine_readable_version(version: OusiaVersion) {
+fn print_machine_readable_version() {
     // TODO
 }
