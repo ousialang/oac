0049e8361930ad8538e7d2fdb4203ce4a2b5b721
Filippo Costa <filippo@neysofu.me>
2024-11-17 13:54:18 +0100

    Hello world test

--- crates/oac/src/qbe_backend.rs
+++ crates/oac/src/qbe_backend.rs
@@ -2,7 +2,7 @@ use qbe::*;
 
 use crate::parser::Ast;
 
-pub fn compile(ast: Ast) -> qbe::Module<'static> {
+pub fn compile(_ast: Ast) -> qbe::Module<'static> {
     let mut module = qbe::Module::default();
 
     module.add_data(qbe::DataDef::new(
@@ -46,4 +46,52 @@ pub fn compile(ast: Ast) -> qbe::Module<'static> {
 
     module.add_function(func);
     module
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use crate::{parser, qbe_backend, tokenizer};
+    use std::io::Write;
+
+    #[test]
+    fn execution_tests() {
+        let test_files = fs::read_dir("execution_tests").unwrap();
+
+        for path in test_files {
+            let tmp = tempfile::tempdir().unwrap();
+
+            let path = path.unwrap().path();
+
+            let source = std::fs::read_to_string(&path).unwrap();
+            let tokens = tokenizer::tokenize(source).unwrap();
+            let ast = parser::parse(tokens).unwrap();
+            let qbe_ir = qbe_backend::compile(ast);
+
+            let qbe_ir_filepath = tmp.path().join("test.qbe");
+
+            let mut file = std::fs::File::create(&qbe_ir_filepath).unwrap();
+            file.write_all(qbe_ir.to_string().as_bytes()).unwrap();
+
+            let assembly_filepath = tmp.path().join("test.s");
+            // run qbe -o $path $path.s && cc $path.s
+            std::process::Command::new("qbe")
+                .arg("-o")
+                .arg(&assembly_filepath)
+                .arg(&qbe_ir_filepath)
+                .output()
+                .unwrap();
+            std::process::Command::new("cc")
+                .arg(&assembly_filepath)
+                .output()
+                .unwrap();
+
+            // run the executable and store the output in a string
+            let output = std::process::Command::new("./a.out").output().unwrap();
+            let output = String::from_utf8(output.stdout).unwrap();
+
+            insta::assert_snapshot!(path.display().to_string(), output);
+        }
+    }
 }
