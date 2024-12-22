21c652a7634c91b36c038731a97831b681910148
Filippo Costa <filippo@neysofu.me>
2024-12-23 00:55:41 +0100

    Add failing scope test

--- crates/oac/src/main.rs
+++ crates/oac/src/main.rs
@@ -5,6 +5,7 @@ mod qbe_backend;
 mod tokenizer;
 
 use std::env;
+use std::path::Path;
 use std::str::FromStr;
 
 use clap::Parser;
@@ -22,46 +23,52 @@ fn main() -> anyhow::Result<()> {
     match oac.subcmd {
         OacSubcommand::Build(build) => {
             let current_dir = std::env::current_dir()?;
-            let target_dir = current_dir.join("target").join("oac");
-            std::fs::create_dir_all(&target_dir)?;
-
-            let source = std::fs::read_to_string(build.source)?;
-            trace!(source_len = source.len(), "Read input file");
-
-            let tokens = tokenizer::tokenize(source)?;
-            let tokens_path = target_dir.join("tokens.json");
-            std::fs::write(&tokens_path, serde_json::to_string_pretty(&tokens)?)?;
-            trace!(tokens_path = %tokens_path.display(), "Tokenized source file");
-
-            let ast = parser::parse(tokens)?;
-            let ast_path = target_dir.join("ast.json");
-            std::fs::write(&ast_path, serde_json::to_string_pretty(&ast)?)?;
-            debug!(ast_path = %ast_path.display(), "Parsed source file");
-
-            let ir = ir::resolve(ast.clone())?;
-            let ir_path = target_dir.join("ir.json");
-            std::fs::write(&ir_path, serde_json::to_string_pretty(&ir)?)?;
-            info!(ir_path = %ir_path.display(), "IR generated and type-checked");
-
-            let qbe_ir = qbe_backend::compile(ir);
-            let qbe_ir_path = target_dir.join("ir.qbe");
-            std::fs::write(&qbe_ir_path, qbe_ir.to_string())?;
-
-            let assembly_path = target_dir.join("assembly.s");
-            std::process::Command::new("qbe")
-                .arg("-o")
-                .arg(&assembly_path)
-                .arg(&qbe_ir_path)
-                .output()?;
-
-            let executable_path = target_dir.join("app");
-            std::process::Command::new("cc")
-                .arg(&assembly_path)
-                .arg("-o")
-                .arg(&executable_path)
-                .output()?;
+            compile(&current_dir, build)?;
         }
     }
+
+    Ok(())
+}
+
+fn compile(current_dir: &Path, build: Build) -> anyhow::Result<()> {
+    let target_dir = current_dir.join("target").join("oac");
+    std::fs::create_dir_all(&target_dir)?;
+
+    let source = std::fs::read_to_string(build.source)?;
+    trace!(source_len = source.len(), "Read input file");
+
+    let tokens = tokenizer::tokenize(source)?;
+    let tokens_path = target_dir.join("tokens.json");
+    std::fs::write(&tokens_path, serde_json::to_string_pretty(&tokens)?)?;
+    trace!(tokens_path = %tokens_path.display(), "Tokenized source file");
+
+    let ast = parser::parse(tokens)?;
+    let ast_path = target_dir.join("ast.json");
+    std::fs::write(&ast_path, serde_json::to_string_pretty(&ast)?)?;
+    debug!(ast_path = %ast_path.display(), "Parsed source file");
+
+    let ir = ir::resolve(ast.clone())?;
+    let ir_path = target_dir.join("ir.json");
+    std::fs::write(&ir_path, serde_json::to_string_pretty(&ir)?)?;
+    info!(ir_path = %ir_path.display(), "IR generated and type-checked");
+
+    let qbe_ir = qbe_backend::compile(ir);
+    let qbe_ir_path = target_dir.join("ir.qbe");
+    std::fs::write(&qbe_ir_path, qbe_ir.to_string())?;
+
+    let assembly_path = target_dir.join("assembly.s");
+    std::process::Command::new("qbe")
+        .arg("-o")
+        .arg(&assembly_path)
+        .arg(&qbe_ir_path)
+        .output()?;
+
+    let executable_path = target_dir.join("app");
+    std::process::Command::new("cc")
+        .arg(&assembly_path)
+        .arg("-o")
+        .arg(&executable_path)
+        .output()?;
 
     Ok(())
 }
--- crates/oac/src/qbe_backend.rs
+++ crates/oac/src/qbe_backend.rs
@@ -244,7 +244,7 @@ fn compile_expr(
 mod tests {
     use std::fs;
 
-    use crate::{ir, parser, qbe_backend, tokenizer};
+    use crate::{compile, ir, parser, qbe_backend, tokenizer, Build};
     use std::io::Write;
 
     #[test]
@@ -252,36 +252,30 @@ mod tests {
         let test_files = fs::read_dir("execution_tests").unwrap();
 
         for path in test_files {
+            let path = path.unwrap().path();
             let tmp = tempfile::tempdir().unwrap();
 
-            let path = path.unwrap().path();
-
-            let source = std::fs::read_to_string(&path).unwrap();
-            let tokens = tokenizer::tokenize(source).unwrap();
-            let ast = parser::parse(tokens).unwrap();
-            let ir = ir::resolve(ast).unwrap();
-            let qbe_ir = qbe_backend::compile(ir);
-
-            let qbe_ir_filepath = tmp.path().join("test.qbe");
-
-            let mut file = std::fs::File::create(&qbe_ir_filepath).unwrap();
-            file.write_all(qbe_ir.to_string().as_bytes()).unwrap();
-
-            let assembly_filepath = tmp.path().join("test.s");
-            // run qbe -o $path $path.s && cc $path.s
-            std::process::Command::new("qbe")
-                .arg("-o")
-                .arg(&assembly_filepath)
-                .arg(&qbe_ir_filepath)
-                .output()
-                .unwrap();
-            std::process::Command::new("cc")
-                .arg(&assembly_filepath)
-                .output()
-                .unwrap();
+            match compile(
+                &tmp.path(),
+                Build {
+                    source: path.to_string_lossy().to_string(),
+                },
+            ) {
+                Ok(()) => (),
+                Err(err) => {
+                    insta::assert_snapshot!(
+                        path.display().to_string(),
+                        format!("COMPILATION ERROR\n\n{err}")
+                    );
+                    continue;
+                }
+            };
 
             // run the executable and store the output in a string
-            let output = std::process::Command::new("./a.out").output().unwrap();
+            let output = std::process::Command::new("./target/oac/app")
+                .current_dir(tmp.path())
+                .output()
+                .unwrap();
             let output = String::from_utf8(output.stdout).unwrap();
 
             insta::assert_snapshot!(path.display().to_string(), output);
